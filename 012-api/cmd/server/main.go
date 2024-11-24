da57969c65a1efc36e2af944babee7176217bb65
Matheus Schenatto <matheus.schenatto@metadados.com.br>
2024-11-24 20:30:26 -0300

    acces token implementation

--- 012-api/cmd/server/main.go
+++ 012-api/cmd/server/main.go
@@ -1,10 +1,64 @@
 package main
 
-import "github.com/math-schenatto/go-projects/api/configs"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	"github.com/math-schenatto/go-projects/api/configs"
+	"github.com/math-schenatto/go-projects/api/internals/entity"
+	"github.com/math-schenatto/go-projects/api/internals/infra/database"
+	"github.com/math-schenatto/go-projects/api/internals/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	_, err := configs.LOadConfig(".")
+	configs, err := configs.LOadConfig(".")
 	if err != nil {
 		panic(err)
 	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("jwtExpiresIn", configs.JwtExperesIn))
+	//r.Use(LogRequest)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJwt)
+
+	//http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
---
+++ 012-api/internals/webserver/handlers/user_handler.go
@@ -0,0 +1,85 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/jwtauth"
+	"github.com/math-schenatto/go-projects/api/internals/dto"
+	"github.com/math-schenatto/go-projects/api/internals/entity"
+	"github.com/math-schenatto/go-projects/api/internals/infra/database"
+)
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(userDB database.UserInterface) *UserHandler {
+	return &UserHandler{
+		UserDB: userDB,
+	}
+}
+
+func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
+	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
+	jwtExpiresIn := r.Context().Value("jwtExpiresIn").(int)
+
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		err := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, tokenString, _ := jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
+	})
+	accessToken := dto.GetJWTOutput{AccessToken: tokenString}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
+
+}
+
+func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
