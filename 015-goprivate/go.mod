ea0187f84339391aef6e3c573c9199ce3238cd69
Matheus Schenatto <matheus.schenatto@metadados.com.br>
2024-12-11 20:56:10 -0300

    graphql

---
+++ 015-goprivate/go.mod
@@ -0,0 +1,3 @@
+module github.com/math-schenatto/fcutils-secret
+
+go 1.21.0
---
+++ 016-graphql/graph/resolver.go
@@ -0,0 +1,12 @@
+package graph
+
+import "github.com/math-schenatto/goprojects/016-grapshql/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+	CourseDB   *database.Course
+}
