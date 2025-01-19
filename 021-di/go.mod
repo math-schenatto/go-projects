bf140778aba9cf8fae2bf914812304c92846bde0
Matheus Schenatto <matheus.schenatto@metadados.com.br>
2025-01-19 19:21:52 -0300

    DI

---
+++ 021-di/go.mod
@@ -0,0 +1,8 @@
+module github.com/math-schenatto/go-projects/021-di
+
+go 1.21.0
+
+require (
+	github.com/google/wire v0.6.0
+	github.com/mattn/go-sqlite3 v1.14.24
+)
---
+++ 021-di/wire.go
@@ -0,0 +1,24 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"database/sql"
+
+	"github.com/google/wire"
+	"github.com/math-schenatto/go-projects/021-di/product"
+)
+
+var setRepositoryDependency = wire.NewSet(
+	product.NewProductRepository,
+	wire.Bind(new(product.ProductRepositoryInterface), new(*product.ProductRepository)),
+)
+
+func NewUseCase(db *sql.DB) *product.ProductUseCase {
+	wire.Build(
+		setRepositoryDependency,
+		product.NewProductUseCase,
+	)
+	return &product.ProductUseCase{}
+}
